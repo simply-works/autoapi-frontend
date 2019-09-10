1a268cf84c855fd56067afa25846df22c20f654c
Ashuverma3162 <ashuverma3162@gmail.com>
2019-09-10 18:44:45 +0530

    changes api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  baseURL: 'http://ec2-3-9-97-225.eu-west-2.compute.amazonaws.com',
+  baseURL: 'https://2t4mtivkmb.execute-api.eu-west-2.amazonaws.com/staging',
   awsmobile : {
     aws_project_region: 'eu-west-2',
     aws_cognito_region: 'eu-west-2',
     aws_user_pools_id: 'eu-west-2_Vp0hZRuA8',
     aws_user_pools_web_client_id: '43b083auborldt90hvb08pefth',
-    domain: 'autoapi.auth.eu-west-2.amazoncognito.com',
-    redirectUrl: 'http://localhost:4200/dashboard',
-    logoutRedirectUrl: 'http://localhost:4200/home',
+    domain: 'https://autoapi.auth.eu-west-2.amazoncognito.com',
+    redirectUrl: 'https://dpdgmov3wklu2.cloudfront.net/dashboard',
+    logoutRedirectUrl: 'https://dpdgmov3wklu2.cloudfront.net/home',
     tokenUrl: 'https://autoapi.auth.eu-west-2.amazoncognito.com/oauth2/token',
     logoutUrl: 'https://autoapi.auth.eu-west-2.amazoncognito.com/logout'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  baseURL: 'http://ec2-3-9-97-225.eu-west-2.compute.amazonaws.com',
+  baseURL: 'https://2t4mtivkmb.execute-api.eu-west-2.amazonaws.com/staging',
   awsmobile : {
     aws_project_region: 'eu-west-2',
     aws_cognito_region: 'eu-west-2',
     aws_user_pools_id: 'eu-west-2_Vp0hZRuA8',
     aws_user_pools_web_client_id: '43b083auborldt90hvb08pefth',
     domain: 'https://autoapi.auth.eu-west-2.amazoncognito.com',
-    redirectUrl: 'http://localhost:4200/dashboard',
-    logoutRedirectUrl: 'http://localhost:4200/home',
+    redirectUrl: 'https://dpdgmov3wklu2.cloudfront.net/dashboard',
+    logoutRedirectUrl: 'https://dpdgmov3wklu2.cloudfront.net/home',
     tokenUrl: 'https://autoapi.auth.eu-west-2.amazoncognito.com/oauth2/token',
     logoutUrl: 'https://autoapi.auth.eu-west-2.amazoncognito.com/logout'
   }
